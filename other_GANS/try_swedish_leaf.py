3a2f0076e9b7f1772b9a611d76f27621cce1f764
gitaar9 <thijs.eker@gmail.com>
2018-12-12 16:09:12 +0100

    rewrote my swedish leave import file to do the same as the one in Utils

--- other_GANS/try_swedish_leaf.py
+++ other_GANS/try_swedish_leaf.py
@@ -1,35 +1,74 @@
 import os
 from PIL import Image
 import numpy as np
+import random
+from datetime import datetime
 
 
-def load_swedish_leaf_in_right_format(size):
-    path = "./datasets/swedish_leaf"
+def read_and_scale_image(image_path, size):
+    image = Image.open(image_path)
+    image = image.resize(size, Image.ANTIALIAS)
+    return np.array(image)
+
+
+def load_one_class(class_path, size, split):
+    image_filenames = os.listdir(class_path)
+
+    # Load images and labels into arrays
+    images = [read_and_scale_image(os.path.join(class_path, filename), size) for filename in image_filenames]
+    labels = [int(filename.split('nr')[0][1:]) for filename in image_filenames]
+
+    # Shuffle the arrays
+    zipped = list(zip(images, labels))
+    random.shuffle(zipped)
+    images, labels = zip(*zipped)
+
+    # Split the arrays
+    split_idx = int(len(labels) * split)
+    return images[:split_idx], labels[:split_idx], images[split_idx:], labels[split_idx:]
+
+
+def load_swedish_leaf_in_right_format(path, size, split):
     classes = os.listdir(path)
     print('All classes: {}'.format(classes))
-    images = []
-    labels = []
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+
     for idx, class_name in enumerate(classes):
         print("({}/15) Loading {} images...".format(idx + 1, class_name))
-        path_to_class_dir = os.path.join(path, class_name)
-        image_filenames = os.listdir(path_to_class_dir)
-        for filename in image_filenames:
-            image_path = os.path.join(path_to_class_dir, filename)
-            image = Image.open(image_path)
-            image = image.resize(size, Image.ANTIALIAS)
-            np_image = np.array(image)
-            images.append(np_image)
-            labels.append(int(filename.split('nr')[0][1:]))
-    return np.array(images), np.array(labels)
+        (x_train_temp, y_train_temp, x_test_temp, y_test_temp) = load_one_class(os.path.join(path, class_name), size, split)
+        x_train.extend(x_train_temp)
+        y_train.extend(y_train_temp)
+        x_test.extend(x_test_temp)
+        y_test.extend(y_test_temp)
+
+    # Final shuffle of the arrays
+    idx = np.random.permutation(len(y_train))
+    x_train, y_train = np.array(x_train)[idx], np.array(y_train)[idx]
+    idx = np.random.permutation(len(y_test))
+    x_test, y_test = np.array(x_test)[idx], np.array(y_test)[idx]
+
+    return x_train, y_train, x_test, y_test
 
 
+# Parameters
+DATADIR = './datasets/swedish_leaf'
+NPY_STORAGE = '../other_GANS/datasets/swedish_np/'
 size = (64, 64,)
-(x, y) = load_swedish_leaf_in_right_format(size)
+split = .8
 
-# Xtrain = [images, height, width, channels]
-print(x.shape)
-# y_train = [labels]
-print(y.shape)
+# Run the functions above...
+random.seed(datetime.now())
+(x_train, y_train, x_test, y_test) = load_swedish_leaf_in_right_format(DATADIR, size, split)
 
-np.save('swedish_leaf{}x{}pix_images'.format(size[0], size[1]), x)
-np.save('swedish_leaf{}x{}pix_labels'.format(size[0], size[1]), y)
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+np.save('{}swedish_leaf{}x{}pix_train_images'.format(NPY_STORAGE, size[0], size[1]), x_train)
+np.save('{}swedish_leaf{}x{}pix_train_labels'.format(NPY_STORAGE, size[0], size[1]), y_train)
+np.save('{}swedish_leaf{}x{}pix_test_images'.format(NPY_STORAGE, size[0], size[1]), x_test)
+np.save('{}swedish_leaf{}x{}pix_test_labels'.format(NPY_STORAGE, size[0], size[1]), y_test)
