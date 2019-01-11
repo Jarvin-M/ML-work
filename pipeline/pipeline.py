4c7d4f21aa9d494b2e9cf67d1a0a5a665c091e29
gitaar9 <thijs.eker@gmail.com>
2019-01-11 16:33:03 +0100

    new run for swedish

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 import numpy as np
 from acgan64_for_pipeline import ACGAN
-from alexnet64_jarvin import AlexNet
+from alexnet64_thijs import AlexNet
 
 
 def split_class(images, labels, split):
@@ -138,4 +138,4 @@ class Pipeline:
 if __name__ == '__main__':
     split = float(sys.argv[1])
     pipe = Pipeline(folder="{}_split_{}".format(datetime.now().strftime("%d_%m_%Y"), str(split).replace('.', '')))
-    pipe.n_runs(n=8, split=split, gan_epochs=50000, alexnet_epochs=400,  alexnet_lr=0.00001)
+    pipe.n_runs(n=6, split=split, gan_epochs=50000, alexnet_epochs=600,  alexnet_lr=0.00001)
